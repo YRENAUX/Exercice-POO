0127493e1ae54a24da1b8524a9053d1af3aa923b
Faarged <l.stoklosa@simplon-charleville.fr>
2019-08-05 14:04:29 +0200

    ajout index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  include_once 'Controller/QuestionController.php';
+  $questioncontroller = new QuestionController();
+  $result = $questioncontroller->lireData();
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>QCM</title>
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+
+  </body>
+</html>
